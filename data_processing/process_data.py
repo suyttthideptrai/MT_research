33dade373a0acfdc68d56b83d6d702e7af28ccb7
Thành <2101040168@ms.hanu.edu.vn>
2024-10-12 19:09:34 +0700

    update 12/10 temp

--- data_processing/process_data.py
+++ data_processing/process_data.py
@@ -13,9 +13,27 @@ import pandas as pd
 import numpy as np
 import csv
 
+SRC_COLUMN = 'Source'
+TGT_COLUMN = 'Target'
+CORPUS_COLUMN = 'Label'
+
+DEFAULT_DEV_ROW = 2000
+DEFAULT_TEST_ROW = 2000
+
+main_df = pd.DataFrame(columns=[SRC_COLUMN, TGT_COLUMN, CORPUS_COLUMN])
+corpus_dict = {}
+
+
 def get_dirname_by_path(file_path):
     """ Get directory out of file path """
     return os.path.dirname(file_path)
+
+
+def get_corpus_name_by_path(file_path):
+    """ Get corpus name """
+    f_name = os.path.splitext(os.path.basename(file_path))[0]
+    return os.path.splitext(f_name)[0]
+
 
 def unzip_corpus(corpus_file_path, output_path):
     # Check if the zip file exists
@@ -28,6 +46,7 @@ def unzip_corpus(corpus_file_path, output_path):
         zip_ref.extractall(output_path)
         print(f"Unzipped '{corpus_file_path}' to '{output_path}' successfully.")
 
+
 def get_data_filepath_by_language_id(directory_path, lang_id):
     """ Get data file by language id """
     for file in os.listdir(directory_path):
@@ -35,89 +54,111 @@ def get_data_filepath_by_language_id(directory_path, lang_id):
             return directory_path + os.path.sep + file
     raise FileNotFoundError(f"Data file with language id '{lang_id}' not found in '{directory_path}', program exit.")
 
-def filter_data(data_frame, corpus_no, src_lang_id, tgt_lang_id, result_path):
+
+def filter_corpus_dict(file_paths):
+    """ Filter corpus names for further processing """
+    global corpus_dict
+    cp_num = 1
+    for cp_path in file_paths:
+        corpus_name = get_corpus_name_by_path(cp_path)
+        print(corpus_name)
+        corpus_dict[cp_num] = corpus_name
+        cp_num += 1
+    for key, value in corpus_dict.items():
+        print(f"Corpus {key}: {value}")
+
+
+def filter_data(df, corpus_no):
+    """ Filter training data in data frame """
     # Delete nan
-    data_frame = data_frame.dropna()
-    print("--- Rows with Empty Cells Deleted\t--> Rows:", data_frame.shape[0])
-
+    print(f"Filtering data for corpus no: {corpus_no}")
+    df = df.dropna()
+    print("--- Rows with Empty Cells Deleted\t--> Rows:", df.shape[0])
     # Drop duplicates
-    data_frame = data_frame.drop_duplicates()
-    print("--- Duplicates Deleted\t\t\t--> Rows:", data_frame.shape[0])
-
+    df = df.drop_duplicates()
+    print("--- Duplicates Deleted\t\t\t--> Rows:", df.shape[0])
     # Drop copy-source rows
-    data_frame["Source-Copied"] = data_frame['Source'] == data_frame['Target']
-    data_frame = data_frame.set_index(['Source-Copied'])
+    df["Source-Copied"] = df[SRC_COLUMN] == df[TGT_COLUMN]
+    df = df.set_index(['Source-Copied'])
     try:  # To avoid (KeyError: '[True] not found in axis') if there are no source-copied cells
-        data_frame = data_frame.drop([True])  # Boolean, not string, do not add quotes
+        df = df.drop([True])  # Boolean, not string, do not add quotes
     except:
         pass
-
-    data_frame = data_frame.reset_index()
-    data_frame = data_frame.drop(['Source-Copied'], axis=1)
-    print("--- Source-Copied Rows Deleted\t\t--> Rows:", data_frame.shape[0])
-
+    df = df.reset_index()
+    df = df.drop(['Source-Copied'], axis=1)
+    print("--- Source-Copied Rows Deleted\t\t--> Rows:", df.shape[0])
     # Drop too-long rows (source or target)
     # Based on your language, change the values "2" and "200"
-    data_frame["Too-Long"] = ((data_frame['Source'].str.count(' ') + 1) > (
-            data_frame['Target'].str.count(' ') + 1) * 2) | \
-                             ((data_frame['Target'].str.count(' ') + 1) > (
-                                     data_frame['Source'].str.count(' ') + 1) * 2) | \
-                             ((data_frame['Source'].str.count(' ') + 1) > 200) | \
-                             ((data_frame['Target'].str.count(' ') + 1) > 200)
-    data_frame = data_frame.set_index(['Too-Long'])
+    df["Too-Long"] = ((df[SRC_COLUMN].str.count(' ') + 1) > (
+            df[TGT_COLUMN].str.count(' ') + 1) * 2) | \
+                     ((df[TGT_COLUMN].str.count(' ') + 1) > (
+                             df[SRC_COLUMN].str.count(' ') + 1) * 2) | \
+                     ((df[SRC_COLUMN].str.count(' ') + 1) > 200) | \
+                     ((df[TGT_COLUMN].str.count(' ') + 1) > 200)
+    df = df.set_index(['Too-Long'])
     try:  # To avoid (KeyError: '[True] not found in axis') if there are no too-long cells
-        data_frame = data_frame.drop([True])  # Boolean, not string, do not add quotes
+        df = df.drop([True])  # Boolean, not string, do not add quotes
     except:
         pass
-
-    data_frame = data_frame.reset_index()
-    data_frame = data_frame.drop(['Too-Long'], axis=1)
-    print("--- Too Long Source/Target Deleted\t--> Rows:", data_frame.shape[0])
-
+    df = df.reset_index()
+    df = df.drop(['Too-Long'], axis=1)
+    print("--- Too Long Source/Target Deleted\t--> Rows:", df.shape[0])
     # Remove HTML and normalize
     # Use str() to avoid (TypeError: expected string or bytes-like object)
     # Note: removing tags should be before removing empty cells because some cells might have only tags and become empty.
-    data_frame = data_frame.replace(r'<.*?>|&lt;.*?&gt;|&?(amp|nbsp|quot);|{}', ' ', regex=True)
-    data_frame = data_frame.replace(r'  ', ' ', regex=True)  # replace double-spaces with one space
-    print("--- HTML Removed\t\t\t--> Rows:", data_frame.shape[0])
-
+    df = df.replace(r'<.*?>|&lt;.*?&gt;|&?(amp|nbsp|quot);|{}', ' ', regex=True)
+    df = df.replace(r'  ', ' ', regex=True)  # replace double-spaces with one space
+    print("--- HTML Removed\t\t\t--> Rows:", df.shape[0])
     # Replace empty cells with NaN
-    data_frame = data_frame.replace(r'^\s*$', np.nan, regex=True)
+    df = df.replace(r'^\s*$', np.nan, regex=True)
     # Delete nan (already there, or generated from the previous steps)
-    data_frame = data_frame.dropna()
-    print("--- Rows with Empty Cells Deleted\t--> Rows:", data_frame.shape[0])
+    df = df.dropna()
+    print("--- Rows with Empty Cells Deleted\t--> Rows:", df.shape[0])
+    df = df.reset_index(drop=True)
+    #
+    # # Shuffle the data
+    # df = df.sample(frac=1).reset_index(drop=True)
+    # print("--- Rows Shuffled\t\t\t--> Rows:", df.shape[0])
+    return df
 
-    # Shuffle the data
-    data_frame = data_frame.sample(frac=1).reset_index(drop=True)
-    print("--- Rows Shuffled\t\t\t--> Rows:", data_frame.shape[0])
 
-    if corpus_no < 0:
-        name_sep = "_"
-    else:
-        name_sep = "_" + corpus_no + "_"
+def write_files(corpus_id, output_file_path, src_lang_id, tgt_lang_id):
+    """ Write """
+    global main_df, corpus_dict
 
+    # Filter out data by corpus id
+    df_filtered_by_c_id = main_df[main_df[CORPUS_COLUMN] == corpus_id]
+    corpus_name = corpus_dict.get(corpus_id)
     path_separator = os.path.sep
-
+    # Get file names
+    source_file = (output_file_path + path_separator
+                   + corpus_name + "_" + corpus_id + "_processed." + src_lang_id)
+    target_file = (output_file_path + path_separator
+                   + corpus_name + "_" + corpus_id + "_processed." + tgt_lang_id)
     # Write the dataframe to two Source and Target files
-    source_file = result_path + path_separator + "corpus" + name_sep + "filtered." + src_lang_id
-    target_file = result_path + path_separator + "corpus" + name_sep + "filtered." + tgt_lang_id
+    df_filtered_by_c_id[[SRC_COLUMN]].to_csv(source_file, header=False, index=False,
+                                             quoting=csv.QUOTE_NONE, sep="\n")
+    print("Source File Saved:", source_file)
+    df_filtered_by_c_id[[TGT_COLUMN]].to_csv(target_file, header=False, index=False,
+                                             quoting=csv.QUOTE_NONE, sep="\n")
+    print("Target File Saved:", target_file)
 
-    # Save source and target to two text files
-    df_source = data_frame["Source"]
-    df_target = data_frame["Target"]
 
-    df_source.to_csv(source_file, header=False, index=False, quoting=csv.QUOTE_NONE, sep="\n")
-    print("--- Source Saved:", source_file)
-    df_target.to_csv(target_file, header=False, index=False, quoting=csv.QUOTE_NONE, sep="\n")
-    print("--- Target Saved:", target_file)
+def handle_write_result_files(src_lang_id, tgt_lang_id, output_file_path, dev_num, test_num):
+    """ Write processed result files """
+    print(f"Processing validation and test data (rows): {DEFAULT_DEV_ROW}, {DEFAULT_TEST_ROW}")
+    print(f"{src_lang_id} + {tgt_lang_id} + {output_file_path}")
+    for key in corpus_dict:
+        write_files(key, output_file_path, src_lang_id, tgt_lang_id)
 
-def process(src_id, tgt_id, dev_perc, zip_paths, output_path, is_split=True):
+
+def process(src_id, tgt_id, dev_num, test_num, zip_paths, output_path):
     """ Process Data """
-    main_df = pd.DataFrame()
-    corpus_no = 1
-    if is_split:
-        corpus_no = -1
+    global main_df, corpus_dict
+    corpus_no = 0
+    sample_corpus_paths = []
     for zip_path in zip_paths:
+        corpus_no += 1
         print(f"Processing corpus no {corpus_no} : {os.path.basename(zip_path)}")
         directory_path = get_dirname_by_path(zip_path)
         unzip_corpus(zip_path, directory_path)
@@ -125,39 +166,67 @@ def process(src_id, tgt_id, dev_perc, zip_paths, output_path, is_split=True):
         tgt_file = get_data_filepath_by_language_id(directory_path, tgt_id)
         print(f"Source file: {src_file}")
         print(f"Target file: {tgt_file}")
-        merge_data(src_file, tgt_file, main_df)
-        filter_data(main_df, corpus_no, src_id, tgt_id, output_path)
-        corpus_no += 1
+        filter_and_merge_data(src_file, tgt_file, corpus_no)
+        sample_corpus_paths.append(src_file)
+    filter_corpus_dict(sample_corpus_paths)
+    print(f"Total corpus: {corpus_no}")
+    print(corpus_dict)
+    print(f"Rows after processed {corpus_no} corpus: {main_df.shape[0]}")
+    handle_write_result_files(src_id, tgt_id, output_path)
 
 
-def merge_data(source_file, target_file, data_frame):
-    df_source = pd.read_csv(source_file, names=['Source'], sep="\0", quoting=csv.QUOTE_NONE, skip_blank_lines=False,
+def filter_and_merge_data(source_file, target_file, corpus_no):
+    """ Process & Merge data to main dataset """
+    global main_df
+    df_source = pd.read_csv(source_file, names=[SRC_COLUMN], sep="\0", quoting=csv.QUOTE_NONE, skip_blank_lines=False,
                             on_bad_lines="skip")
-    df_target = pd.read_csv(target_file, names=['Target'], sep="\0", quoting=csv.QUOTE_NONE, skip_blank_lines=False,
+    df_target = pd.read_csv(target_file, names=[TGT_COLUMN], sep="\0", quoting=csv.QUOTE_NONE, skip_blank_lines=False,
                             on_bad_lines="skip")
     if len(df_source) != len(df_target):
         raise ValueError("Source and target files have different number of lines")
+    #Concat two data columns
     df_corpus = pd.concat([df_source, df_target], axis=1)
-    print(f"Corpus dataframe shape (rows, columns): {df_corpus.shape}")
-    data_frame = pd.concat([data_frame, df_corpus], axis=0)
-    print(f"Main dataframe shape (rows, columns): {data_frame.shape}")
+    #Filter data
+    df_corpus = filter_data(df_corpus, corpus_no)
+    #Concat corpus label for file output handling
+    df_label = pd.DataFrame({CORPUS_COLUMN: [corpus_no] * len(df_corpus)})
+    df_corpus = pd.concat([df_corpus, df_label], axis=1)
+    print(f"Processed corpus dataframe shape (rows, columns): {df_corpus.shape}")
+    columns_to_print = [SRC_COLUMN, TGT_COLUMN]
+    print(f"5 rows from head of corpus {corpus_no}: ")
+    print(df_corpus[columns_to_print].head(5))
+    print(f"5 rows from tail of corpus {corpus_no}: ")
+    print(df_corpus[columns_to_print].tail(5))
+    main_df = pd.concat([main_df, df_corpus], axis=0)
+    print(f"Total rows after merging corpus {corpus_no}: {main_df.shape[0]}")
 
 
-path = "../data/OpenSubtitiles/en-vi.txt.zip"
-dir_path = get_dirname_by_path("../data/OpenSubtitiles/en-vi.txt.zip")
-print(str(path))
-process("en", "vi", 0.5, [path])
+def validate_and_create_dir(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
 
+# path = "../data/QED/en-vi.txt.zip"
+# path1 = "../data/Wikimedia/en-vi.txt.zip"
+# dir_path = get_dirname_by_path("../data/QED/en-vi.txt.zip")
+# print(str(path))
+# result_path = "../data/filtered/"
+# validate_and_create_dir(result_path)
+# process("en", "vi", 0.5, [path, path1], result_path)
+
+# cp_name = get_corpus_name_by_path("../data/QED/QED.en-vi.en")
+# print(cp_name)
 
 
-
-# if __name__ == '__main__':
-#     src_lang_id = sys.argv[1]
-#     tgt_lang_id = sys.argv[2]
-#     percentage = float(sys.argv[3])
-#     if percentage < 0.0 or percentage > 1.0:
-#         print("Error: percentage must be between 0.0 and 1.0")
-#         exit(1)
-#     zip_file_paths = sys.argv[4:]
-#     print(f"params: {src_lang_id}, {tgt_lang_id}, {percentage}, {zip_file_paths}")
+if __name__ == '__main__':
+    src_lang_id = sys.argv[1]
+    tgt_lang_id = sys.argv[2]
+    dev_row = sys.argv[3]
+    test_row = sys.argv[4]
+    result_path = sys.argv[5]
+    zip_file_paths = sys.argv[6:]
+    print(f"params: {src_lang_id}, {tgt_lang_id}, {dev_row}, {test_row}, {zip_file_paths}")
+    start_time = time.time()
+    process(src_lang_id, tgt_lang_id, dev_row, test_row, zip_file_paths, result_path)
+    end_time = time.time()
+    print(f"Done data processing in {end_time - start_time} seconds")
--- draft/test.py
+++ draft/test.py
@@ -5,27 +5,44 @@ from spacy.lang.vi.examples import sentences
 from pos_tagging import constants as const
 
 
-def spacy_test():
-    nlp = spacy.load(const.SPACY_TAGGING_MODELS["vi"])
+# def spacy_test():
+#     nlp = spacy.load(const.SPACY_TAGGING_MODELS["vi"])
+#
+#     # nlp = spacy.load("zh_core_web_md-3.8.0-py3-none-any.whl")
+#     doc = nlp(sentences[0])
+#     print(doc.text)
+#     # Print the headers
+#     print(f"{'Token':<15} {'Lemma':<15} {'POS':<10} {'Tag':<10} {'Dep':<15} {'Shape':<10} {'Is Alpha':<10} {'Is Stop'}")
+#
+#     # Iterate over tokens in the document
+#     for token in doc:
+#         print(f"{token.text:<15} {token.lemma_:<15} {token.pos_:<10} {token.tag_:<10} {token.dep_:<15} "
+#               f"{token.shape_:<10} {token.is_alpha:<10} {token.is_stop}")
+#
+# def read_file(file_path):
+#     print(f"Reading file from: {file_path}")
+#     df = pd.read_csv(file_path, sep='\0', header=None, skip_blank_lines=False, on_bad_lines="skip")
+#     print("Dataframe shape (rows, columns):", df.shape)
+#
+#
+# if __name__ == '__main__':
+#     read_file("../test_data/test_data.en")
 
-    # nlp = spacy.load("zh_core_web_md-3.8.0-py3-none-any.whl")
-    doc = nlp(sentences[0])
-    print(doc.text)
-    # Print the headers
-    print(f"{'Token':<15} {'Lemma':<15} {'POS':<10} {'Tag':<10} {'Dep':<15} {'Shape':<10} {'Is Alpha':<10} {'Is Stop'}")
-
-    # Iterate over tokens in the document
-    for token in doc:
-        print(f"{token.text:<15} {token.lemma_:<15} {token.pos_:<10} {token.tag_:<10} {token.dep_:<15} "
-              f"{token.shape_:<10} {token.is_alpha:<10} {token.is_stop}")
-
-def read_file(file_path):
-    print(f"Reading file from: {file_path}")
-    df = pd.read_csv(file_path, sep='\0', header=None, skip_blank_lines=False, on_bad_lines="skip")
-    print("Dataframe shape (rows, columns):", df.shape)
-
-
-if __name__ == '__main__':
-    read_file("../test_data/test_data.en")
-
+data = {
+    'col1': ['E', 'B', 'F'],
+    'col2': ['A', 'B', 'C'],
+    'col3': ['1', '1', '2']
+}
+df = pd.DataFrame(data)
+print(df.head(3))
+current_cor_idx = "current_processing_index"
+df[current_cor_idx] = df['col3'] == '1'
+df.set_index(current_cor_idx, inplace=True)
+print(df.head(3))
+df['same_source'] = df['col1'] == df['col2']
+print(df.head(3))
+print(df.shape)
+main_df = df.reset_index(drop=True)
+print(df.head(3))
+print(df.shape)
 
